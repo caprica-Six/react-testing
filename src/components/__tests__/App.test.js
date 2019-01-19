0b39290a353dd98f1f5b2ef568b6f516b757724e
caprica-Six <mogwais@outlook.com>
2019-01-19 14:15:01 +0000

    use helper function to avoid repetition - code reuse

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,12 +4,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+// use helper function to avoid repetition with beforeEach()
+beforeEach(() => {
+    const wrapped = shallow(<App />);
+});
+
+
 it('shows a comment box', () => {
     // shallow renders just the component, none of its children
     // use wrapped as per the documentation, same as const component = shallow();
     // ref: https://airbnb.io/enzyme/docs/api/ShallowWrapper/find.html
-
-    const wrapped = shallow(<App />);
 
     // make an expectation & find all the instances of the CommentBox inside
     expect(wrapped.find(CommentBox).length).toEqual(1);
@@ -17,7 +21,5 @@ it('shows a comment box', () => {
 });
 
 it('shows a comment list', () => {
-    const wrapped = shallow(<App />);
-
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
