f351b322666919cc22172c5af2b1cc48494666ca
caprica-Six <mogwais@outlook.com>
2019-01-19 13:26:22 +0000

    test App component only to know Comment Box exists, not the text inside it - not ideal to use expect here...(wip)

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,4 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App';
 
+it('shows a comment box', () => {
+    // fake div that exists only inside the memory with JSDOM (react by default)
+    const div = document.createElement('div');
 
+    ReactDOM.render(<App />, div);
+
+    // Looks inside the fake div
+    // Checks to see if the CommentBox is in there
+    //console.log(div.innerHTML);
+
+    //make sure the COmmentBox exists
+    expect(div.innerHTML).toContain('Comment Box');
+
+    // cleanup after the test runs
+    ReactDOM.unmountComponentAtNode(div);
+});
