8c69599fba5808a6f0e0d67df1da7f7e0681a103
caprica-Six <mogwais@outlook.com>
2019-01-19 14:39:18 +0000

    CommentBox test with Full DOM Enzyme method

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CommentBox from 'components/CommentBox';
+
+/* Tests expectations with FULL DOM Enzyme method:
+
+- Show a text area and a button 
+- Users can enter input into the text area and submit 
+- When the input is submitted, textarea should get emptied 
+*/
+
+
+it('had a text area and a button', () => {
+
+});
