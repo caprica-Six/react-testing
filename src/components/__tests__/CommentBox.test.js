a19d0c7534737802bf8c8e7201d741c8795638e5
caprica-Six <mogwais@outlook.com>
2019-01-19 14:51:11 +0000

    CommentBox - assert elements existance

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,15 +1,20 @@
 import React from 'react'
-
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-/* Tests expectations with FULL DOM Enzyme method:
+/* Test expectations with FULL DOM Enzyme method:
 
 - Show a text area and a button 
 - Users can enter input into the text area and submit 
 - When the input is submitted, textarea should get emptied 
 */
 
+it('has a text area and a button', () => {
+    const wrapped = mount(<CommentBox />);
 
-it('had a text area and a button', () => {
+    // console.log(wrapped.find('textarea'));
+    // console.log(wrapped.find('button'));
 
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
 });
