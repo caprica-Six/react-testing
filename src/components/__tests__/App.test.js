b7809b50a27ef00733f8333d71d776c885d5a62c
caprica-Six <mogwais@outlook.com>
2019-01-19 13:57:07 +0000

    add an expectation with shallow

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
-    // fake div that exists only inside the memory with JSDOM (react by default)
-    const div = document.createElement('div');
+    // shallow renders just the component, none of its children
+    // use wrapped as per the documentation, same as const component = shallow();
+    // ref: https://airbnb.io/enzyme/docs/api/ShallowWrapper/find.html
 
-    ReactDOM.render(<App />, div);
+    const wrapped = shallow(<App />);
 
-    // Looks inside the fake div
-    // Checks to see if the CommentBox is in there
-    //console.log(div.innerHTML);
-
-    //make sure the COmmentBox exists
-    expect(div.innerHTML).toContain('Comment Box');
-
-    // cleanup after the test runs
-    ReactDOM.unmountComponentAtNode(div);
+    // make an expectation & find all the instances of the CommentBox inside
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+    
 });
