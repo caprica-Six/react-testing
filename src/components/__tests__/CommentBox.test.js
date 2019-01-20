1c026b332d2596ff6d2a908a9391665ef270da50
caprica-Six <mogwais@outlook.com>
2019-01-20 19:38:58 +0000

    import Root to CommentBox - tests are now passing...yay

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 /* Test expectations with FULL DOM Enzyme method:
 
@@ -12,7 +13,10 @@ import CommentBox from 'components/CommentBox';
 let wrapped;
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
+    wrapped = mount(
+    <Root>
+        <CommentBox />
+    </Root>);
 });
 
 // cleanup with jest helper afterEach() 
